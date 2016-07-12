25a0c6d248b9f3eb68cbe9220e87df37a7aba27f
LIChing <littleching26@gmail.com>
2016-07-12 22:10:49 +0800

    d

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.get('/', function(request, response) {
 			response.status(406).end();
 		} else {
 			response.type('application/json');
-		response.status(200).send("results:"+docs);
+		response.status(200).send(docs);
 			response.end();
 		}
 	});
