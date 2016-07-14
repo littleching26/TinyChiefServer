2d257b3d2961ed93d3554e68ef1b35056a932172
LIChing <littleching26@gmail.com>
2016-07-14 10:12:51 +0800

    d

--- index.js
+++ index.js
@@ -22,12 +22,12 @@ mongodb.MongoClient.connect(mongodbURL, function(err, db) {
 
 app.get('/', function(request, response) {
 	var collection = myDB.collection('cook_book');
-	collection.find({}).toArray(function(err, docs) {
+	collection.find({"name":"皮蛋地瓜葉(4人份)"}).toArray(function(err, docs) {
 		if (err) {
 			response.status(406).end();
 		} else {
 			response.type('application/json');
-		response.status(200).send(docs);
+			response.status(200).send(docs);
 			response.end();
 		}
 	});
