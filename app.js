516633889932c1abf959d2679a78236be46fc2b6
littleching26 <littleching26@gmail.com>
2016-10-05 19:13:29 +0800

    send

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
 //引用 nodemailer
 var nodemailer = require('nodemailer');
+var express = require('express');
+var myParser = require('body-parser');//這是新加的
+var app = express();
+var mongodb = require('mongodb');
+var express = require('express');
+var mongodbURL = 'mongodb://LIChing:justtheway402@ds021731.mlab.com:21731/tiny_chief';
+var myDB;
+app.set('port', (process.env.PORT || 5000));
+//提高上傳限制
+app.use(myParser({limit: '50mb'}));
 
+mongodb.MongoClient.connect(mongodbURL, function(err, db) {
+	if (err) {
+		console.log(err);
+	} else {
+		myDB = db;
+		console.log('connection success');
+	}
+});
+
+app.use(myParser.urlencoded({extended : true}));	
 //宣告發信物件
 var transporter = nodemailer.createTransport({
     service: 'Gmail',
@@ -23,11 +43,26 @@ var options = {
     
 };
 
-//發送信件方法
-transporter.sendMail(options, function(error, info){
-    if(error){
-        console.log(error);
-    }else{
-        console.log('訊息發送: ' + info.response);
-    }
+app.post('/send',function(req,res){
+    rand=Math.floor((Math.random() * 100) + 54);
+	var collection = myDB.collection('user_account');
+	collection.update({user:acceptac}, {$set: {randNumber:rand}});
+	host=req.get('host');
+	link="http://"+req.get('host')+"/verify?id="+rand;
+	mailOptions={
+		from: '"hahaha 👥" <clownheart5221@gmail.com>', 
+		to : acceptEmail,
+		subject : "Please confirm your Email account",
+		html : "Hello,<br> Please Click on the link to verify your email.<br><a href="+link+">Click here to verify</a>"	
+	}
+	console.log(mailOptions);
+	transporter.sendMail(mailOptions, function(error, response){
+   	 if(error){
+        	console.log(error);
+		res.end("error");
+	 }else{
+        	console.log("Message sent: " + response.message);
+		res.end("sent");
+    	 }
+	});
 });
