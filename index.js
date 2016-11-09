2969b47d7718cda2f5d3ffd5a5fde9cc801197d2
littleching26 <littleching26@gmail.com>
2016-11-10 01:00:17 +0800

    debug

--- index.js
+++ index.js
@@ -312,7 +312,10 @@ app.post('/inserFBInfo', function(request, response){
 	var acceptUSERID = request.body.UserID;
 	var acceptFBName = request.body.FBName;
 	var collection = myDB.collection('user_account');
-	collection.products.insert( { "_id" : ObjectId("5063114bd386d8fadbd6b004"), "nickname":acceptFBName } )
+	collection.insertMany([{"_id": mongodb.ObjectID(acceptUSERID),"nickname":acceptFBName}], function(err, result) {
+	assert.equal(err, null);
+	assert.equal(1, result.result.n);
+	assert.equal(1, result.ops.length);
 	});
 	acceptUSERID = null;
 	acceptFBName = null;
