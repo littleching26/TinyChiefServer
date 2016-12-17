c08784b516871ad4215d7e0a2e4770cc8f2654c8
littleching26 <littleching26@gmail.com>
2016-12-17 22:23:16 +0800

    加註解,刪redundant

--- index.js
+++ index.js
@@ -31,35 +31,47 @@ mongodb.MongoClient.connect(mongodbURL, function(err, db) {
 
 app.use(myParser.urlencoded({extended : true}));	
 
-
+//登入
 app.post('/login', function(request, response){
+	//接收使用者帳號
 	acceptac = request.body.User;
+	//接收使用者密碼
 	acceptwd = request.body.Password;
     console.log(acceptac);
 	console.log(acceptwd);
+	//宣告user_account
 	var collection = myDB.collection('user_account');
+	//找到對應的帳號密碼
 	collection.find({"user":acceptac,"password":acceptwd}).toArray(function(err, docs) {
 		if (err) {
 			response.status(406).end();
 		} else {
+			//找到後回傳
 			response.type('application/json');
 			response.status(200).send(docs);
 			response.end();
+			//將之設為空
 			acceptac = null;
 			acceptwd = null;
 		}
 	});
 });
-
+//註冊
 app.post('/register', function(request, response){
+	//接收帳號
 	acceptac = request.body.User;
+	//接收密碼
 	acceptwd = request.body.Password;
+	//接收EMAIL
 	acceptEmail = request.body.myEmail;
+	//接收暱稱
 	acceptNickname = request.body.nickName;
     console.log(acceptac);
 	console.log(acceptwd);
 	console.log(acceptEmail);
+	//宣告user_account
 	var collection = myDB.collection('user_account');
+	//找到對應的user
 	collection.find({"user":acceptac}).toArray(function(err, docs) {
 		if (err) {
 			response.status(406).end();
@@ -67,6 +79,7 @@ app.post('/register', function(request, response){
 			response.type('application/json');
 			response.status(200).send(docs);
 			response.end();
+			//若找不到代表此帳號無人使用 如此此帳號可使用
 			if(JSON.stringify(docs)=="[]"){
 				insertDocuments(myDB, function() {
 				});
@@ -76,7 +89,7 @@ app.post('/register', function(request, response){
 		}
 	});
 });
-
+//插入使用者資料
 var insertDocuments = function(myDB){
 	var collection = myDB.collection('user_account');
 	collection.insertMany([{user : acceptac,password : acceptwd,email : acceptEmail,checkEmail:"NO",nickname:acceptNickname}], function(err, result) {
@@ -85,7 +98,7 @@ var insertDocuments = function(myDB){
 	assert.equal(1, result.ops.length);
 	});
 }
-
+//宣告發信物件
 var transporter = nodemailer.createTransport({
     service: 'Gmail',
     auth: {
@@ -93,13 +106,19 @@ var transporter = nodemailer.createTransport({
         pass: 'esogoodx06justtheway526'
     }
 });
-
+//送信
 app.post('/send',function(req,res){
+	//宣告一個亂數
     rand=Math.floor((Math.random() * 1000) + 54);
+	//宣告資料庫user_account的資料表
 	var collection = myDB.collection('user_account');
+	//將亂數加入指定user以便日後認證
 	collection.update({user:acceptac}, {$set: {randNumber:rand}});
+	//取得host address
 	host=req.get('host');
+	//鏈結網址
 	link="http://"+req.get('host')+"/verify?id="+rand;
+	//寄信內容
 	mailOptions={
 		from: '"智慧料理小當家" <clownheart5221@gmail.com>', 
 		to : acceptEmail,
@@ -107,6 +126,7 @@ app.post('/send',function(req,res){
 		html : "<br> 請點擊連結以認證信箱！<br><a href="+link+">前去認證</a>"	
 	}
 	console.log(mailOptions);
+	//發送郵件
 	transporter.sendMail(mailOptions, function(error, response){
    	 if(error){
         	console.log(error);
@@ -117,17 +137,21 @@ app.post('/send',function(req,res){
     	 }
 	});
 });
-
+//認證
 app.get('/verify',function(req,res){
 	console.log(req.protocol+":/"+req.get('host'));
+	//確認信箱的人是否與申請者的host相同
 	if((req.protocol+"://"+req.get('host'))==("http://"+host))
 	{
 		console.log("Domain is matched. Information is from Authentic email");
+		//若亂數相同
 		if(req.query.id==rand)
 		{
 			console.log("email is verified");
+			//show出網頁的內容
 			res.end("<h1> "+mailOptions.to+" 已成功認證囉！<br>現在您可以開始享受您最輕鬆上手的APP-智慧料理小當家");
 			var collection = myDB.collection('user_account');
+			//更改資料表中對應rand之帳號的checkEmail
 			collection.update({randNumber:rand}, {$set: {checkEmail:"OK"}});
 		}
 		else
@@ -140,32 +164,6 @@ app.get('/verify',function(req,res){
 	{
 		res.end("<h1>Request is from unknown source");
 	}
-});
-
-transporter.sendMail(mailOptions, function(error, info){
-	if(error){
-		return console.log(error);
-	}
-	console.log('Message sent: ' + info.response);
-});
-
-app.use(myParser.urlencoded({extended : true}));
-app.post('/createCookBook', function(request, response){
-	console.log(request.body); 
-	  var cookBookTitle = request.body.title;
-	  var arrIngredients = request.body.Ingredients.split("|");
-	  var arrStep = request.body.Step.split("|");
-	  var cookBookImage = request.body.image;
-	  console.log(request.body.title);
-	  console.log(arrIngredients);
-	  console.log(arrStep);
-      response.end();
-	var collection = myDB.collection('cook_book');
-	collection.insertMany([{count:0,title:cookBookTitle,ingredients:arrIngredients,steps:arrStep,image:cookBookImage}], function(err, result) {
-	assert.equal(err, null);
-	assert.equal(1, result.result.n);
-	assert.equal(1, result.ops.length);
-	});
 });
 
 app.post('/cookbook/detail', function(request, response){
@@ -254,11 +252,14 @@ var setAvgRate = function(id){
                             { upsert : true});
     });
 };
-
+//查詢食譜
 app.post('/search/result', function(request, response){
+	//接收查詢string
 	var acceptSearchTitle= request.body.SearchTitle;
     console.log(acceptSearchTitle);
+	//宣告cook_book這table
 	var collection = myDB.collection('cook_book');
+	//搜尋關鍵字並有模糊搜尋的功能 之後將搜尋之結果按照rate_avg之數遞減排列
 	collection.find({"title":{'$regex': acceptSearchTitle, $options: 'i'}}).sort({"rate_avg":-1}).toArray(function(err, docs) {
 		if (err) {
 			response.status(406).end();
@@ -283,13 +284,17 @@ app.get('/get/price', function(request, response) {
 		}
 	});
 });
-
+//將臉書登入之帳號及名稱加入資料庫
 app.post('/inserFBInfo', function(request, response){
+	//接收使用者ID
 	var acceptUSERID = request.body.UserID;
+	//接收臉書名稱
 	var acceptFBName = request.body.FBName;
 	console.log(acceptUSERID);
 	console.log(acceptFBName);
+	//宣告user_account此table
 	var collection = myDB.collection('user_account');
+	//插入使用者資料
 	collection.insert({"nickname":acceptFBName},function(err, doc) {
 		if (err) {			
             console.log(err);
@@ -297,14 +302,21 @@ app.post('/inserFBInfo', function(request, response){
             response.end();
 		} 
         else {
+			//新的id
 			var newid;
+			//舊的id
 			var origid;
 			console.log(doc.insertedIds[0]);
+			//id在doc.insertedIds陣列裡是第一筆資料
 			origid = doc.insertedIds[0];
+			//在table中找到剛剛插入那筆資料的_id
 			newid = collection.findOne({'_id':origid});
+			//將新的id設成剛剛接收到的使用者ID
 			newid._id = acceptUSERID;
+			//將新的id之nickname設成剛剛接收到的使用者名稱
 			newid.nickname = acceptFBName;
 			console.log(newid);
+			//插入資料表
 			collection.insert(newid,function(err,doc){
 				if(err) {
 					console.log(err);
@@ -317,6 +329,7 @@ app.post('/inserFBInfo', function(request, response){
 					response.end();
 				}
 			});
+			//將舊的id那筆資料刪掉
 			collection.remove({'_id':origid})
 		}
 	});
